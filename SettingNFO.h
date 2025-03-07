2489a4660c207a5e8e399f0a1ef2f2880390574f
Jeffrey Wilts <36136544+jwilts@users.noreply.github.com>
2025-03-06 22:23:27 -0500

    Create SettingNFO.h
    Initial Commit for SettingNFO.h

---
+++ SettingNFO.h
@@ -0,0 +1,19 @@
+// SettingNFO.h
+
+#ifndef SETTINGNFO_H
+#define SETTINGNFO_H
+
+// Email settings
+const char* AUTHOR_EMAIL = "YourEmail@gmail.com";
+const char* AUTHOR_PASSWORD = "GMAIL Application Key";
+const char* RECIPIENT_EMAIL = "WhoToNotify@hotmail.com";
+
+/** The smtp host name e.g. smtp.gmail.com for GMail or smtp.office365.com for Outlook or smtp.mail.yahoo.com */
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 587
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "WhoToNotify@hotmail.com"
+
+
+#endif // SETTINGNFO_H
